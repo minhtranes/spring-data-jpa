6a1046fb4a739f21cf651d002446f93d51847932
Minh Trần <wisekingtran@gmail.com>
2018-04-07 00:16:54 +0700

    Configure project one datasource

---
+++ spring-data-jpa-example-multiple-datasource/src/main/java/vn/minhht/study/springdatajpa/infrastructure/config/ds/ProjectOneDataSource.java
@@ -0,0 +1,59 @@
+/*
+ * Class: ProjectOneDatasource
+ *
+ * Created on Apr 6, 2018
+ *
+ * (c) Copyright Swiss Post Solution, unpublished work
+ * All use, disclosure, and/or reproduction of this material is prohibited
+ * unless authorized in writing.  All Rights Reserved.
+ * Rights in this program belong to:
+ * Swiss Post Solution.
+ * Floor 4-5-8, ICT Tower, Quang Trung Software City
+ */
+package vn.minhht.study.springdatajpa.infrastructure.config.ds;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import vn.minhht.study.springdatajpa.infrastructure.persistence.entity.hr.Department;
+import vn.minhht.study.springdatajpa.infrastructure.persistence.repository.hr.DepartmentRepository;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(entityManagerFactoryRef = "ds1EntityManagerFactory"
+, transactionManagerRef = "ds1TransactionManager", basePackageClasses = {
+    DepartmentRepository.class })
+public class ProjectOneDataSource {
+
+    @Bean(name = "ds1DataSource")
+    @ConfigurationProperties(prefix = "datasource.ds1")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "ds1EntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean barEntityManagerFactory(
+        EntityManagerFactoryBuilder builder,
+        @Qualifier("ds1DataSource") DataSource dataSource) {
+        return builder.dataSource(dataSource).packages(Department.class)
+            .persistenceUnit("ds1").build();
+    }
+
+    @Bean(name = "ds1TransactionManager")
+    public PlatformTransactionManager ds1TransactionManager(
+        @Qualifier("ds1EntityManagerFactory") EntityManagerFactory barEntityManagerFactory) {
+        return new JpaTransactionManager(barEntityManagerFactory);
+    }
+}
---
+++ spring-data-jpa-example-multiple-datasource/src/main/java/vn/minhht/study/springdatajpa/infrastructure/persistence/entity/hr/Department.java
@@ -0,0 +1,78 @@
+/*
+ * Class: Department
+ *
+ * Created on Apr 6, 2018
+ *
+ * (c) Copyright Swiss Post Solution, unpublished work
+ * All use, disclosure, and/or reproduction of this material is prohibited
+ * unless authorized in writing.  All Rights Reserved.
+ * Rights in this program belong to:
+ * Swiss Post Solution.
+ * Floor 4-5-8, ICT Tower, Quang Trung Software City
+ */
+package vn.minhht.study.springdatajpa.infrastructure.persistence.entity.hr;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Department implements Serializable {
+
+    private static final long serialVersionUID = 3717794186655236580L;
+
+    private Long id;
+
+    private String name;
+
+    private Employee manager;
+
+    private List<Employee> employees = new ArrayList<>();
+
+    @Id
+    @SequenceGenerator(name = "department_seq", initialValue = 1, allocationSize = 1, sequenceName = "department_id_seq")
+    @GeneratedValue(generator = "department_seq", strategy = GenerationType.SEQUENCE)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    @OneToMany(mappedBy = "department", fetch = FetchType.LAZY)
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
